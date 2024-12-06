b15ac923b767543bf19e366c4db5d336e857baa2
chanminkim-K <snoopy0813@naver.com>
2024-12-06 09:50:41 +0900

    feature: Redis AOP 적용

---
+++ src/main/java/com/megazone/ERPSystem_phase3_LogisticsProduction/common/config/aop/RedisCacheAspect.java
@@ -0,0 +1,4 @@
+package com.megazone.ERPSystem_phase3_LogisticsProduction.common.config.aop;
+
+public class RedisCacheAspect {
+}
---
+++ src/main/java/com/megazone/ERPSystem_phase3_LogisticsProduction/common/config/redis/RedisService.java
@@ -0,0 +1,26 @@
+package com.megazone.ERPSystem_phase3_LogisticsProduction.common.config;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public RedisService(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void set(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String get(String key) {
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+}
